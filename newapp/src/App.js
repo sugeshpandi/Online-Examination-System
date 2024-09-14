f6a2181338519a65b8e33116bf2b3e6435d7c193
sugeshpandi <sugeshpandisaravana@gmail.com>
2024-09-14 12:07:36 +0530

    feat:css applied for login page

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Login from "./Login";
-import Register from "./Register";
+// import Register from "./Register";
 
 function App() {
   return (
     <div>
       <Login />
-      <Register />
+      {/* <Register /> */}
     </div>
 
   );
