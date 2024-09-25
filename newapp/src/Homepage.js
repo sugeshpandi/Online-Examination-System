8084cb44b8d3c78a3b9dcadcc68cd91610e8074d
sugeshpandi <sugeshpandisaravana@gmail.com>
2024-09-25 21:34:46 +0530

    css

--- newapp/src/Homepage.js
+++ newapp/src/Homepage.js
@@ -7,16 +7,16 @@ const Homepage = () => {
 			<nav className="navbar">
 				<ul>
 					<li>
-					<h1 className='logo'>Online-Examination</h1>
-					</li>
-					<li className='button'>
-					Login
+						<h1 className='logo'>Online-Examination</h1>
 					</li>
 				</ul>
 			</nav>
 			<section className='content'>
 				<h1>Hi buddy!</h1>
-				<h3>Welcome to the Examination.</h3>
+				<h3>Welcome to the <span>Examination</span> .</h3>
+			</section>
+			<section className='button'>
+				<button>Login</button>
 			</section>
 		</main>
 	);
